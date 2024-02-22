7f616f623c7c7a09105b9e2122e8f9f1ac11ea2c
Nick Ezekiel Madu <nickezekiel@ignislab.com>
2024-02-22 11:15:08 +0100

    renamed folder src/locale to src/locales

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,6 +1,6 @@
 import { createI18n } from 'vue-i18n'
-import en from '@/locale/en.json'
-import fr from '@/locale/fr.json'
+import en from '@/locales/en.json'
+import fr from '@/locales/fr.json'
 
 const i18n = createI18n({
   legacy: false, // you must set `false`, to use Composition API
